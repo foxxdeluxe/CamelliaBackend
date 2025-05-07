19d8425346f00e4c4755f3ee96818a234c4de0ca
Alexander <knockbackml@gmail.com>
2025-05-07 13:23:45 +0800

    Use submodules for some libraries.

--- helper/algorithm_helper.cpp
+++ helper/algorithm_helper.cpp
@@ -2,37 +2,41 @@
 // Created by LENOVO on 2025/4/2.
 //
 
+#include "camellia.h"
+#include "xxHash/xxhash.h"
 #include <algorithm>
 #include <cmath>
-#include "camellia.h"
-#include "xxhash/xxhash.h"
+
 
 namespace camellia::algorithm_helper {
-    boolean_t approx_equals(number_t a, number_t b) {
-        auto tolerance = std::max({1.0F, std::fabsf(a), std::fabsf(b)});
-        return std::fabsf(a - b) <= std::numeric_limits<float>::epsilon() * tolerance;
-    }
+boolean_t approx_equals(number_t a, number_t b) {
+    auto tolerance = std::max({1.0F, std::fabsf(a), std::fabsf(b)});
+    return std::fabsf(a - b) <=
+           std::numeric_limits<float>::epsilon() * tolerance;
+}
 
-    integer_t get_bbcode_string_length(const text_t& bbcode) {
-        integer_t res = 0;
-        for (int i = 0; i < bbcode.length(); i++) {
-            res++;
-            if (bbcode[i] != '[') continue;
+integer_t get_bbcode_string_length(const text_t &bbcode) {
+    integer_t res = 0;
+    for (int i = 0; i < bbcode.length(); i++) {
+        res++;
+        if (bbcode[i] != '[')
+            continue;
 
-            auto j = i + 1;
-            while (j < bbcode.length() && bbcode[j] != ']') j++;
-            if (j >= bbcode.length()) {
-                res += j - i - 1;
-                break;
-            }
-
-            i = j;
+        auto j = i + 1;
+        while (j < bbcode.length() && bbcode[j] != ']')
+            j++;
+        if (j >= bbcode.length()) {
+            res += j - i - 1;
+            break;
         }
 
-        return res;
+        i = j;
     }
 
-    hash_t calc_hash(const std::string &str) {
-        return XXH64(str.data(), str.length(), 11451);
-    }
+    return res;
 }
+
+hash_t calc_hash(const std::string &str) {
+    return XXH64(str.data(), str.length(), 11451);
+}
+} // namespace camellia::algorithm_helper
